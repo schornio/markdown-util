9bb1b8218abf657c3d0f7864a701a2be8f25da2a
Thomas Schorn <t@schorn.io>
2023-09-18 17:49:54 +0200

    [major] compile to js (without x)

--- next.js
+++ next.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Markdown = exports.heading = void 0;
+const jsx_runtime_1 = require("react/jsx-runtime");
 const _1 = require("./");
 const image_1 = __importDefault(require("next/image"));
 const link_1 = __importDefault(require("next/link"));
@@ -12,20 +13,20 @@ function heading(depthOffset) {
     const Heading = ({ children, content, }) => {
         const depth = Math.min(content.depth + depthOffset, 6);
         const HeadingComponent = `h${depth}`;
-        return <HeadingComponent>{children}</HeadingComponent>;
+        return (0, jsx_runtime_1.jsx)(HeadingComponent, { children: children });
     };
     return Heading;
 }
 exports.heading = heading;
 function Markdown({ children, componentsOverride, headingTopLevel = 2, imageHeight = 300, imageWidth = 600, }) {
-    const components = (0, react_1.useMemo)(() => (Object.assign({
-        heading: heading(headingTopLevel - 1),
-        image: ({ content }) => {
-            var _a;
-            return (<image_1.default alt={(_a = content.alt) !== null && _a !== void 0 ? _a : ""} height={imageHeight} width={imageWidth} src={content.url}/>);
+    const components = (0, react_1.useMemo)(() => ({
+        ...{
+            heading: heading(headingTopLevel - 1),
+            image: ({ content }) => ((0, jsx_runtime_1.jsx)(image_1.default, { alt: content.alt ?? "", height: imageHeight, width: imageWidth, src: content.url })),
+            link: ({ children, content }) => ((0, jsx_runtime_1.jsx)(link_1.default, { href: content.url, children: children })),
         },
-        link: ({ children, content }) => (<link_1.default href={content.url}>{children}</link_1.default>),
-    }, componentsOverride)), [headingTopLevel, imageHeight, imageWidth, componentsOverride]);
-    return <_1.Markdown components={components}>{children}</_1.Markdown>;
+        ...componentsOverride,
+    }), [headingTopLevel, imageHeight, imageWidth, componentsOverride]);
+    return (0, jsx_runtime_1.jsx)(_1.Markdown, { components: components, children: children });
 }
 exports.Markdown = Markdown;
